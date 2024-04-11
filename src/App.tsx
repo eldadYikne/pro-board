9f6906dfdb9b7be8817a63bf7319b9e97a389f43
eldadYikne <nmknv99@gmail.com>
2024-04-11 15:30:39 +0300

    add connect

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,46 @@
-import React from "react";
+import React, { useEffect } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import Navbar from "./components/Navbar";
 import ConfirmedPlace from "./components/ConfirmedPlace";
 import { BrowserRouter, Route, Link, Routes } from "react-router-dom";
 import Map from "./components/Map";
+import mongoose from "mongoose";
+
 function App() {
+  let cached = (global as any).mongoose;
+  const MONGODB_URI =
+    "mongodb+srv://reutyikne:iY9LJ5G5sD8xl5Ts@cluster0.cuxuy5g.mongodb.net/test";
+  if (!cached) {
+    cached = (global as any).mongoose = { conn: null, promise: null };
+  }
+  useEffect(() => {
+    async function fetchData() {
+      await dbConnect();
+    }
+    fetchData();
+  }, []);
+  async function dbConnect() {
+    if (cached.conn) {
+      return cached.conn;
+    }
+
+    if (!cached.promise) {
+      const opts = {
+        bufferCommands: false,
+      };
+
+      cached.promise = mongoose.connect(MONGODB_URI, opts).then((mongoose) => {
+        return mongoose;
+      });
+    }
+    cached.conn = await cached.promise;
+    mongoose.set("strictQuery", false);
+    await mongoose.connect(cached.conn);
+
+    return cached.conn;
+  }
+
   return (
     <div dir="rtl" className="">
       <BrowserRouter>
